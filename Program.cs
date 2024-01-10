c22171c4e4ff565fdd77c384e26daaec8a8762e0
Bogdan <bohdansadovskiy@gmail.com>
2024-01-10 20:43:59 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Repeat;
+
+public static class REPEAT
+{
+    public static int Main(string[] args)
+    {
+        FirstExercise.firstEx();
+        return 0;
+    }
+}
---
+++ Regex.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Repeat
+{
+    internal class RegexStr
+    {
+        private static string patternForNumbers = "^([\\d])+$";
+        private static bool isMatch(string str, string pattern)
+        {
+            if (str == null) return false;
+            if (Regex.IsMatch(str, pattern)) return true;
+            return false;
+        }
+        public static bool isOnlyNumbers(string str)
+        {
+            return isMatch(str, patternForNumbers);
+        }
+    }
+}
